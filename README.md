def3dee7ff9380cadfac5e1aac7a15c5054a5650
Connor Nelson <Connor@ConnorNelson.com>
2021-07-09 15:51:42 -0700

    Rename for pypi

---
+++ README.md
@@ -0,0 +1,5 @@
+# qtterm
+
+Forked from [https://gitlab.com/mikeramsey/py3qtermwidget/-/tree/master/](https://gitlab.com/mikeramsey/py3qtermwidget/-/tree/master/)
+
+That project was GPLv3, so I guess this is too?
--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="py3qterm",
-    version="0.4",
-    author="Michael Ramsey",
-    author_email="mike@hackerdise.me",
-    description="Simple terminal/console widget for PyQt5/Pyside2 with vt100 support based on pyqtermwidget (https://bitbucket.org/henning/pyqtermwidget) Original Author: Henning Schroeder ",
-    url="https://gitlab.com/mikeramsey/py3qtermwidget",
+    name="qtterm",
+    version="0.5",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
